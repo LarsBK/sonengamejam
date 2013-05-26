5bb0bcc4ccbbd42edf5434501de36773fe8b7567
LarsBK <lars.bjorlykke.kristiansen@gmail.com>
2013-05-26 03:12:09 +0300

    Update Trigger.js

--- Trigger.js
+++ Trigger.js
@@ -4,6 +4,20 @@ var Trigger = cc.Node.extend({
        this._rect = new cc.Rect(0,0,10,20)//todo fix
    },
    onColide:function(other){
+      
+      if(other == this.getParent().player){
        this.getParent().trigger(this.t);
+      }
    }
 });
+
+var Lava = cc.Layer.extend({
+   onColide:function(other){
+      if(other == this.getParent().player){
+         this.getParent().trigger({action:"die"})
+      }
+   },
+   update:function(dt){
+      this.setSizeY( 10*dt)
+   }
+})
